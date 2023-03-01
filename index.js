df13fe92625d9c0f936a762072f4ab4d8490b5bb
James Macpherson <jmx@aukman.com>
2023-03-01 12:22:36 +0100

    simplify setup

--- index.js
+++ index.js
@@ -9,25 +9,28 @@ fs.readFile("config.json", {encoding: 'utf-8'}, function(configReadError, data) 
     const user = userManager.addUser(credentials.username, credentials.password, true)
     const privilegeManager = new webdav.SimplePathPrivilegeManager();
 
-    privilegeManager.setRights(user, '/sync', ['all']);
+    privilegeManager.setRights(user, '/', ['all']);
 
     const server = new webdav.WebDAVServer({
         port: 1900,
         httpAuthentication: new webdav.HTTPBasicAuthentication(userManager),
         privilegeManager: privilegeManager,
-        requireAuthentification: true
+        requireAuthentification: true,
+        rootFileSystem: new webdav.PhysicalFileSystem('sync')
     });
 
     server.afterRequest((arg, next) => {
         // Display the method, the URI, the returned status code and the returned message
         console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
         // If available, display the body of the response
-        console.log(arg.responseBody);
+        // console.log(arg.responseBody);
         next();
     });
-    
-    server.setFileSystem('/sync', new webdav.PhysicalFileSystem('sync'), (success) => {
-        server.start(() => console.log('READY'));
-    });
-    
+
+    server.start(() => console.log('READY'));
+
+    // server.setFileSystem('sync', new webdav.PhysicalFileSystem('sync'), (success) => {
+        // server.start(() => console.log('READY'));
+    // });
+    // 
 });
