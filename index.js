c87c297d6ee9eb11a1b00ce34741b786ee327bec
James Macpherson <jmx@aukman.com>
2023-03-01 10:49:46 +0100

    working sync webdav

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-// TypeScript
-import { v2 as webdav } from 'webdav-server'
+import { v2 as webdav } from 'webdav-server';
+import fs from 'fs';
 
-const server = new webdav.WebDAVServer({
-    port: 1900
+fs.readFile("config.json", {encoding: 'utf-8'}, function(configReadError, data) {
+    if (configReadError) throw configReadError;
+
+    const userManager = new webdav.SimpleUserManager();
+    const credentials = JSON.parse(data);
+    const user = userManager.addUser(credentials.username, credentials.password, true)
+    const privilegeManager = new webdav.SimplePathPrivilegeManager();
+
+    privilegeManager.setRights(user, '/', ['all']);
+
+    const server = new webdav.WebDAVServer({
+        port: 1900,
+        httpAuthentication: new webdav.HTTPBasicAuthentication(userManager),
+        privilegeManager: privilegeManager,
+        requireAuthentification: true
+    });
+
+    server.afterRequest((arg, next) => {
+        // Display the method, the URI, the returned status code and the returned message
+        console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
+        // If available, display the body of the response
+        console.log(arg.responseBody);
+        next();
+    });
+    
+    server.setFileSystem('/', new webdav.PhysicalFileSystem('sync'), (success) => {
+        server.start(() => console.log('READY'));
+    });
+    
 });
- 
-server.afterRequest((arg, next) => {
-    // Display the method, the URI, the returned status code and the returned message
-    console.log('>>', arg.request.method, arg.requested.uri, '>', arg.response.statusCode, arg.response.statusMessage);
-    // If available, display the body of the response
-    console.log(arg.responseBody);
-    next();
-});
- 
-server.start(() => console.log('READY'));
